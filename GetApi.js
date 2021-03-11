bf9556ae3f6446b27ad8b637a24b59c1a85bb42d
Nawal Bhengra <nawalbhengra02@gmail.com>
2021-03-11 13:20:41 +0530

    Dashboard, Header, Router-Dom installed

---
+++ GetApi.js
@@ -0,0 +1,24 @@
+var urlString = "";
+
+const GetApiCall = {
+  getRequest(url) {
+
+   return fetch(urlString+url, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Access-Control-Allow-Origin':'*',
+        'Access-Control-Allow-Headers' : '*',
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(response => {
+      // console.log(response)
+      return(response)
+     
+    })
+    .catch(error => { console.log('request failed', error); 
+    return error;
+  });
+  },
+};
