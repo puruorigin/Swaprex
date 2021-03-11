fb71fb182e4e6eb01076140b36e0387ef6130b00
Nawal Bhengra <nawalbhengra02@gmail.com>
2021-03-11 11:40:51 +0530

    dashboard

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import Footer from './Footer';
 import Header from './Header';
 import OwlCarousel from 'react-owl-carousel';
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';
-
 
 class Dashboard extends Component {
     componentDidMount() {
